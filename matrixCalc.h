39d1d7b9b3462a40b03ea9c4d1b8a0e23a4a1ac6
Ninzore <33434617+Ninzore@users.noreply.github.com>
2019-12-13 00:53:35 +0800

    Update matrixCalc.h
    add normal distribution related functions

--- matrixCalc.h
+++ matrixCalc.h
@@ -17,14 +17,32 @@ typedef struct initMat{
 }matrix;
 
 void printMartix(matrix matrix);
+
 matrix createMatrix(int row, int col, double init_val[]);
+
+double _gaussianDistribution(int centre, float std_deviation);
+
+#define gaussianDistribution(centre, std_deviation) \
+_gaussianDistribution(DEFARG(centre, 0), DEFARG(std_deviation, 1))
+
+matrix _createNormalMatrix(int row, int col, int centre, float std_deviation);
+#define createNormalMatrix(row, col, centre, std_deviation) \
+_createNormalMatrix(DEFARG(row, 1), DEFARG(col, 1), DEFARG(centre, 0), DEFARG(std_deviation, 1))
+
 matrix _createRandMatrix(int row, int col, int start, int end);
-#define createRandMatrix(row, col, start, end) _createRandMatrix(DEFARG(row, 1), DEFARG(col, 1), DEFARG(start, 0), DEFARG(end, 1))
+#define createRandMatrix(row, col, start, end) \
+_createRandMatrix(DEFARG(row, 1), DEFARG(col, 1), DEFARG(start, 0), DEFARG(end, 1))
+
 double *toArray(matrix matrix);
+
 matrix transform(matrix mat_a);
+
 matrix plus(matrix mat_a, matrix mat_b);
+
 matrix minus(matrix mat_a, matrix mat_b);
+
 matrix scalar(matrix mat_a, double num);
+
 matrix dot(matrix mat_a, matrix mat_b);
 
 #endif
