1d99483bd6393cf2163e70d4ef0459b0d18b43e5
Ninzore <33434617+Ninzore@users.noreply.github.com>
2019-12-14 00:26:49 +0800

    Update example.c
    change some examples

--- example.c
+++ example.c
@@ -23,9 +23,36 @@ int main(){
   matrix k = createRandMatrix(3,3,0,1);
   printMartix(k);
   
+  //transform the 2d matrix to 1d array, handling the result with a double pointer
+  double *buffer = toArray(m);
+  printf("%f", buffer[0]);
+  //ALWAYS REMEMBER to free
+  free(buffer);
+  
+  //get 1 random number obey the gaussian distribution
+  printf("%f\n", gaussianDistribution(0,1));
+  //default output is centre at 0, standard deviation 1
+  printf("%f\n", gaussianDistribution(,));
+  
+  //get a matrix of random number obey the gaussian distribution, 
+  matrix normal_mat = createNormalMatrix(5, 5, 0, 1);
+  printMartix(normal_mat);
+  //default output is 1*1 matrix with value centre at 0, standard deviation 1
+  matrix normal_mat_ = createNormalMatrix(,,,);
+  printMartix(normal_mat_);
+  
+  //print the inner product of two 1-d matrix(array)
+  printf("%f", inner(m, n));
+  
   //do dot product, handling the result with a matrix structure
   matrix c = dot(m, n);
   printMartix(c);
+  
+  //do dot product for two 1-d matrix(array), give the inner product of them in a 0-d matrix(scalar)
+  matrix s = createNormalMatrix(1,5, arr);
+  matrix d = createNormalMatrix(1,5, arr);
+  matrix f = dot(s, d);
+  printMartix(f);
   
   //do addition
   matrix d = plus(m, n);
@@ -39,21 +66,7 @@ int main(){
   d = transform(d);
   printMartix(d);
   
-  //multiply with an int
+  //multiply with a float
   d = scalar(d, 2);
   printMartix(d);
-  
-  //transform the 2d matrix to 1d array, handling the result with a double pointer
-  double *buffer = toArray(m);
-  printf("%f", buffer[0]);
-  //ALWAYS REMEMBER to free
-  free(buffer);
-  
-  //get 1 random number obey the gaussian distribution
-  //default output is centre at 0, standard deviation 1
-  printf("%f\n", gaussianDistribution(0,1));
-  //get a matrix of random number obey the gaussian distribution, 
-  //default output is 1*1 matrix with value centre at 0, standard deviation 1
-  matrix normal_mat = createNormalMatrix(5, 5, 0, 1);
-  printMartix(normal_mat);
 }
