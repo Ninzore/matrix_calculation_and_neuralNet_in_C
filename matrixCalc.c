acab12f78f137b228a9f4eb898ce09b3c4912079
Ninzore <33434617+Ninzore@users.noreply.github.com>
2019-12-15 01:42:29 +0800

    Update matrixCalc.c
    add sum, expSum, createIdentityMatrix

--- matrixCalc.c
+++ matrixCalc.c
@@ -28,7 +28,7 @@ Args:
     init_val: double list, the list transfer to matrix
                            can be 0;
 
-Output: matrix, if the size of matrix is larger than the
+Return: matrix, if the size of matrix is larger than the
                 init_val, matrix size will not be changed
                 rest of the matrix will be all 0;
 */
@@ -44,7 +44,7 @@ void printMartix(matrix matrix){
 }
 
 /*
-use the row, col and an array to create a matrix type
+use the row, col and an array to create a matrix
 
 Args:  
     row: int, total number of rows of the matrix
@@ -52,7 +52,7 @@ Args:
     init_val: double list, the list transfer to matrix
                            can be 0;
 
-Output: matrix, if the size of matrix is larger than the
+Return: matrix, if the size of matrix is larger than the
                 init_val, matrix size will not be changed
                 rest of the matrix will be all 0;
 */
@@ -69,6 +69,24 @@ matrix createMatrix(int row, int col, double init_val[]){
 }
 
 /*
+create an identity matrix
+
+Args:  
+    row: int, total number of rows of the matrix
+
+Return: matrix, the identity matrix
+*/
+matrix createIdentityMatrix(int n){
+    matrix mat;
+    mat.row = n;
+    mat.col = n;
+    for (int i=0; i<n; i++){
+        mat.val[i][i] = 1;
+    }
+    return mat;
+}
+
+/*
 generate a random matrix with shape row*col
 REMEMBER to call srand((unsigned) time(0)) only ONCE before using
 default output 1*1 matrix with vals within 0~1
@@ -80,7 +98,7 @@ Args:
     start: int, the minium of values
     end: int, the maxium of values
 
-Output: matrix type
+Return: matrix type
 */
 matrix _createRandMatrix(int row, int col, int start, int end){
     double arr[row*col];
@@ -92,9 +110,8 @@ matrix _createRandMatrix(int row, int col, int start, int end){
         return mat;
     }
     else{
-        printf("error, start >= end\n");
-        matrix mat = createMatrix(1, 1, arr);
-        return mat;
+        printf("error, start >= end");
+        exit(-1);
     }
 }
 #define createRandMatrix(row, col, start, end) _createRandMatrix(DEFARG(row, 1), DEFARG(col, 1), DEFARG(start, 0), DEFARG(end, 1))
@@ -105,8 +122,8 @@ use Box-Muller method to generate number in Gaussian (normal) distrubution
 Args:
     centre: int, set the centre
     std_deviation: float, the standard deviation of the distrubution
-    
-Output: double the random number generated
+
+Return: double the random number generated
 */
 double _gaussianDistribution(int centre, float std_deviation){
     double PI = 3.1415926535897932;
@@ -119,7 +136,8 @@ double _gaussianDistribution(int centre, float std_deviation){
     z = (radius * sin(theta) + centre) * std_deviation;
     return z;
 }
-#define gaussianDistribution(centre, std_deviation) _gaussianDistribution(DEFARG(centre, 0), DEFARG(std_deviation, 1))
+#define gaussianDistribution(centre, std_deviation) \
+_gaussianDistribution(DEFARG(centre, 0), DEFARG(std_deviation, 1))
 
 /*
 use Box-Muller method to generate a matrix with values follow Gaussian (normal) distrubution
@@ -130,7 +148,7 @@ Args:
     centre: int, set the centre of the distrubution
     std_deviation: float, the standard deviation of the distrubution
              
-Output: mattrix type, with values follows normal distrubution
+Return: mattrix type, with values follows normal distrubution
 */
 matrix _createNormalMatrix(int row, int col, int centre, float std_deviation){
     double arr[row*col];
@@ -149,12 +167,12 @@ array size = matrix.row * matrix.col
 REMEMBER to free the memery after using
 Args:
     matrix: matrix type
-Output:
+Return:
     array: double pointer, use array[] to access individual val 
 */
 double *toArray(matrix matrix){
     //allocate the memery for the array, x8 for double, change to x4 for int or float
-    double *array = malloc(matrix.row * matrix.col * 8));
+    double *array = malloc(matrix.row * matrix.col * 8);
     for (int i=0; i<matrix.row; i++){
         for (int j=0; j<matrix.col; j++){
             array[i * matrix.col + j] = matrix.val[i][j];
@@ -179,6 +197,25 @@ matrix transform(matrix mat_a){
     return mat_b;
 }
 
+
+/*
+transfer all the members in the matrix to exponential form
+Args:
+    matrix_a: matrix type, a matrix
+
+Return: matrix, the exp form of the input matrix
+*/
+matrix expMat(matrix mat_in){
+    double arr[mat_in.row * mat_in.col];
+    for (int i=0; i<mat_in.row; i++){
+        for (int j=0; j<mat_in.col; j++){
+            arr[i * mat_in.col + j] = exp(mat_in.val[i][j]);
+        }
+    }
+    matrix mat_result = createMatrix(mat_in.row, mat_in.col, arr);
+    return mat_result;
+}
+
 /*
 matrix addition
 Args:
@@ -189,8 +226,8 @@ matrix plus(matrix mat_a, matrix mat_b){
     double arr_c[] = {0};
     matrix mat_c = createMatrix(mat_a.row, mat_b.col, arr_c);
     if ((mat_a.row != mat_b.row) || (mat_a.col != mat_b.col)){
-        printf("\nerror, matrix A and matrix B have different sizes");
-        return mat_c;
+        printf("error, matrix A and matrix B have different sizes");
+        exit(-1);
     }
     else{
         for (int i=0; i<mat_a.row; i++){
@@ -212,8 +249,8 @@ matrix minus(matrix mat_a, matrix mat_b){
     double arr_c[] = {0};
     matrix mat_c = createMatrix(mat_a.row, mat_b.col, arr_c);
     if ((mat_a.row != mat_b.row) || (mat_a.col != mat_b.col)){
-        printf("\nerror, matrix A and matrix B have different sizes");
-        return mat_c;
+        printf("error, matrix A and matrix B have different sizes");
+        exit(-1);
     }
     else{
         for (int i=0; i<mat_a.row; i++){
@@ -226,11 +263,45 @@ matrix minus(matrix mat_a, matrix mat_b){
 }
 
 /*
+for calculating the sum of members in a matrix
+Args:
+    matrix_a: matrix type, a matrix
+
+Return: double, the sum of each member in the matrix
+*/
+double sum(matrix mat_in){
+    double result = 0;
+    for (int i=0; i<mat_in.row; i++){
+        for (int j=0; j<mat_in.col; j++){
+            result += mat_in.val[i][j];
+        }
+    }   
+    return result;
+}
+
+/*
+for calculating the sum exponential of members in a matrix
+Args:
+    matrix_a: matrix type, a matrix
+
+Return: double, the sum exp of each member in the matrix
+*/
+double expSum(matrix mat_in){
+    double result = 0;
+    for (int i=0; i<mat_in.row; i++){
+        for (int j=0; j<mat_in.col; j++){
+            result += exp(mat_in.val[i][j]);
+        }
+    }   
+    return result;
+}
+
+/*
 matrix multiply with a scalar value
 Args:
     matrix_a: matrix type, a matrix
     num: double, the number used to multiply
-Output: 
+Return: 
     matrix type, the result matrix
 */
 matrix scalar(matrix matrix_a, double num){
@@ -250,7 +321,7 @@ get the inner product of 2 1-d matrix(array)
 Args:
     mat_a: matrix type, a 1-d matrix(array)
     mat_b: matrix type, a 1-d matrix(array)
-Output: 
+Return: 
     double, the inner product result
 */
 double inner(matrix mat_a, matrix mat_b){
@@ -277,7 +348,7 @@ if they are 2-d matrix, then do matrix multiplication
 Args:
     mat_a: matrix type, a matrix
     mat_b: matrix type, a matrix
-Output: 
+Return: 
     matrix type, the result matrix
 */
 matrix dot(matrix mat_a, matrix mat_b){
@@ -310,4 +381,23 @@ matrix dot(matrix mat_a, matrix mat_b){
         exit(-1);
     }
     return mat_c;
+}
+
+
+matrix reshape(matrix mat_a, int row, int col){
+    matrix mat_b;
+    if (mat_a.row * mat_a.col != row * col){
+        printf("\nerror, the target size is %dx%d but the input matrix size is %dx%d", row, col, mat_a.row, mat_a.col);
+        exit(-1);
+    }
+    else{
+        double arr[row*col];
+        for (int i=0; i<mat_a.row; i++){
+            for (int j=0; j<mat_a.col; j++){
+                arr[i * mat_a.col + j] = mat_a.val[i][j];
+                mat_b = createMatrix(row, col, arr);
+            }
+        }
+    }
+    return mat_b;
 }
