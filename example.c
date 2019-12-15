795ff5f3632b8fb41b6519a224617418317df46f
Ninzore <33434617+Ninzore@users.noreply.github.com>
2019-12-16 00:54:09 +0800

    add examples for new/updated functions

--- example.c
+++ example.c
@@ -6,25 +6,30 @@
 #include <float.h>
 
 int main(){
+  //REMEMBER to use srand BEFORE using!!!
+  srand((unsigned) time(0));
+  
   //create an array for initialization matrix
   double arr[] = {4,7,12,2};
   
   //initialize matrix m and n with given row, col and array
-  matrix m = createMatrix(2,2, arr);
-  matrix n = createMatrix(2,2, arr);
+  matrix m = createMatrix(2, 2, arr);
+  matrix n = createMatrix(2, 2, arr);
   
   //print out the matrixs
   printMartix(m);
   printMartix(n);
+  
+  //create a matrix with all the members are equal to one number
+  matrix z = createMatrix(2, 2, 0);
+  printMartix(z);
   
   //create an identity matrix
   matrix i = createIdentityMatrix(5);
   printMartix(i);
   
   //initialize a random matrix k with given row, col, and range
-  //REMEMBER to use srand BEFORE using!!!
-  srand((unsigned) time(0));
-  matrix k = createRandMatrix(3,3,0,1);
+  matrix k = createRandMatrix(3, 3, 0, 1);
   printMartix(k);
   
   //transform the 2d matrix to 1d array, handling the result with a double pointer
@@ -34,7 +39,7 @@ int main(){
   free(buffer);
   
   //get 1 random number obey the gaussian distribution
-  printf("%f\n", gaussianDistribution(0,1));
+  printf("%f\n", gaussianDistribution(0, 1));
   //default output is centre at 0, standard deviation 1
   printf("%f\n", gaussianDistribution(,));
   
@@ -53,18 +58,26 @@ int main(){
   printMartix(c);
   
   //do dot product for two 1-d matrix(array), give the inner product of them in a 0-d matrix(scalar)
-  matrix s = createNormalMatrix(1,5, arr);
-  matrix d = createNormalMatrix(1,5, arr);
+  matrix s = createNormalMatrix(1, 5, arr);
+  matrix d = createNormalMatrix(1, 5, arr);
   matrix f = dot(s, d);
   printMartix(f);
   
-  //do addition
-  matrix d = plus(m, n);
-  printMartix(d);
+  //add a number to all members in the matrx
+  matrix q = plus(m, 2);
+  printMartix(q);
   
-  //do minus
-  matrix e = minus(m,n);
+  //do matrix addition
+  matrix w = minus(m, 3);
+  printMartix(w);
+  
+  //do matrix addition
+  matrix e = plusMat(m, n);
   printMartix(e);
+  
+  //do matrix minus
+  matrix r = minusMat(m,n);
+  printMartix(r);
   
   //matrix transform
   d = transform(d);
